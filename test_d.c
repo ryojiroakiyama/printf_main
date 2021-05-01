e2f2aafdd4785ccdcfa68a521b7a4bd6b4866893
Ryojiro Akiyama <rakiyama@c1r1s6.42tokyo.jp>
2021-05-01 15:36:44 +0900

    add all

--- test_d.c
+++ test_d.c
@@ -15,6 +15,7 @@ int main(void)
 	printf("-----------------d\n");
 	ret = printf("%d", 123);
 	put("%d", 123, ret);
+
 	ret = printf("%.d", 123);
 	put("%.d", 123, ret);
 	ret = printf("%.d", 0);
@@ -29,46 +30,46 @@ int main(void)
 	put("%.3d", 123, ret);
 	ret = printf("%.5d", 123);
 	put("%.5d", 123, ret);
+
+	ret = printf("%0d", 0);
+	put("%0d", 0, ret);
 	ret = printf("%1d", 123);
 	put("%1d", 123, ret);
 	ret = printf("%5d", 123);
 	put("%5d", 123, ret);
+
 	ret = printf("%10.3d", 123);
 	put("%10.3d", 123, ret);
 	ret = printf("%10.5d", 123);
 	put("%10.5d", 123, ret);
 	ret = printf("%10.15d", 123);
 	put("%10.15d", 123, ret);
+
 	ret = printf("%-10.5d", 123);
 	put("%-10.5d", 123, ret);
 	ret = printf("%------------10.5d", 123);
 	put("%------------10.5d", 123, ret);
+
+	ret = printf("%010d", 123);
+	put("%010d", 123, ret);
+	ret = printf("%010.1d", 123);
+	put("%010.1d", 123, ret);
 	ret = printf("%010.5d", 123);
 	put("%010.5d", 123, ret);
 	ret = printf("%0000000000010.5d", 123);
 	put("%0000000000010.5d", 123, ret);
 	ret = printf("%010.5d", 123);
 	put("%010.5d", 123, ret);
+
 	ret = printf("%.*d", -1, 0);
 	put("%.*d , -1", 0, ret);
-	ret = printf("%010.1d", 123);
-	put("%010.1d", 123, ret);
 	ret = printf("%*.*d", 10, -3, 123);
 	put("%*.*d , 10, -3", 123, ret);
 	ret = printf("%*.*d", -10, 5, 123);
 	put("%*.*d , -10, 5", 123, ret);
-	ret = printf("%*d", 0000010, 123);
-	put("%*d , 0000010", 123, ret);
-	ret = printf("%010d", 123);
-	put("%010d", 123, ret);
 	ret = printf("%*d", 0, 123);
 	put("%*d , 0", 123, ret);
-	ret = printf("%0d", 0);
-	put("%0d", 0, ret);
-	printf("koko\n");
-	ret = printf("%2147483647d", 123);
-	put ("%2147483647d", 123, ret);
-//	printf("%d\n", 010);
+
 	//error
 //	ret = printf("%-010.5d", 123);
 //	ret = printf("%-----5----10.5d", 123);
@@ -78,7 +79,11 @@ int main(void)
 //	ret = printf("%.~d", 123);
 //	ret = printf("%**d\n", 5, 123);
 //	ret = printf("%*.**d\n", 10, 5, 123);
-//	ret = printf("%9223372036854775807d", 123);
+//	ret = printf("%2147483647d", 123);//int_max, stop to put
+//	ret = printf("%*d", 0000010, 123);//cp consider about octal
+//	ret = printf("%", 123);
+//	ret = printf("adsf", 123);
+//	ret = printf("%.-5d", 123);
 
 	//use the ret of write to ret of printf?
 	//%%
